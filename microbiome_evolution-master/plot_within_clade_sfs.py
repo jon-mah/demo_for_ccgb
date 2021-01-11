f2db23e19c3801dbbb053cfb4af2ad626570e92c
Jonathan Mah <jonmah@uw.edu>
2021-01-11 15:24:51 -0800

    figure formatting changes for output.

--- microbiome_evolution-master/plot_within_clade_sfs.py
+++ microbiome_evolution-master/plot_within_clade_sfs.py
@@ -1,15 +1,15 @@
-# SFS plots, comparing syn and non syn 
+# SFS plots, comparing syn and non syn
 # (a) regular SFS (percentage of population)
 # (b) Singletons, doubletons, and pi-weighted
 
 # Summary across species?
-# dN/dS vs dS plot? 
-# core vs variable genes? 
+# dN/dS vs dS plot?
+# core vs variable genes?
 
 # Within-snp gene changes
 
-import matplotlib  
-matplotlib.use('Agg') 
+import matplotlib
+matplotlib.use('Agg')
 import config
 import parse_midas_data
 ###
@@ -68,7 +68,7 @@ if other_species:
     other_species_str = "_%s" % species_name
 else:
     other_species_str = ""
-    
+
 ################################################################################
 
 min_coverage = config.min_median_coverage
@@ -83,7 +83,7 @@ sys.stderr.write("Loading sample metadata...\n")
 subject_sample_map = parse_HMP_data.parse_subject_sample_map()
 sample_country_map = parse_HMP_data.parse_sample_country_map()
 sys.stderr.write("Done!\n")
-       
+
 # Only plot samples above a certain depth threshold that are "haploids"
 snp_samples = diversity_utils.calculate_haploid_samples(species_name, debug=debug)
 snp_samples = snp_samples[ diversity_utils.parse_midas_data.calculate_unique_samples(subject_sample_map, snp_samples)]
@@ -120,7 +120,7 @@ sfs_axis.get_yaxis().tick_left()
 
 ##############################################################################
 #
-# Panel (b). Comparison of 1D and 4D for minor allele *count* 
+# Panel (b). Comparison of 1D and 4D for minor allele *count*
 #
 ##############################################################################
 
@@ -150,7 +150,7 @@ sys.stderr.write("Calculating matrix...\n")
 dummy_samples, snp_difference_matrix, snp_opportunity_matrix = calculate_substitution_rates.calculate_matrices_from_substitution_rate_map(substitution_rate_map, 'core', allowed_samples=snp_samples)
 snp_samples = numpy.array(dummy_samples)
 substitution_rate = snp_difference_matrix*1.0/(snp_opportunity_matrix+(snp_opportunity_matrix==0))
-sys.stderr.write("Done!\n")   
+sys.stderr.write("Done!\n")
 
 #################
 #
@@ -172,11 +172,11 @@ largest_clade_samples = coarse_grained_samples[ clade_idxss[clade_sizes.argmax()
 
 sys.stderr.write("Top level: %d clades, %s\n" % (len(clade_sets), str(clade_sizes)))
 sys.stderr.write("Max: %d\n" % len(largest_clade_samples))
- 
+
 if len(largest_clade_samples)<3:
     sys.stderr.write("Too few samples! Quitting...\n")
     sys.exit(1)
-    
+
 sys.stderr.write("Continuing with %d samples...\n" % len(largest_clade_samples))
 
 # Load SNP information for species_name
@@ -194,8 +194,8 @@ synonymous_difference_matrix = numpy.array([])
 synonymous_opportunity_matrix = numpy.array([])
 
 nonsynonymous_difference_matrix = numpy.array([])
-nonsynonymous_opportunity_matrix = numpy.array([])    
-   
+nonsynonymous_opportunity_matrix = numpy.array([])
+
 maf_bins = []
 mafs = []
 
@@ -211,19 +211,19 @@ nonsynonymous_count_sfs = []
 synonymous_pi_weighted_counts = 0
 nonsynonymous_pi_weighted_counts = 0
 
-    
+
 final_line_number = 0
 while final_line_number >= 0:
-    
+
     sys.stderr.write("Loading chunk starting @ %d...\n" % final_line_number)
     snp_samples, allele_counts_map, passed_sites_map, final_line_number = parse_midas_data.parse_snps(species_name, debug=debug, allowed_variant_types=allowed_variant_types, allowed_samples=largest_clade_samples,allowed_genes=core_genes, chunk_size=chunk_size,initial_line_number=final_line_number)
     sys.stderr.write("Done! Loaded %d genes\n" % len(allele_counts_map.keys()))
-    
+
     # Calculate fixation matrix
     sys.stderr.write("Calculating matrix of snp differences...\n")
-    chunk_snp_difference_matrix, chunk_snp_opportunity_matrix = diversity_utils.calculate_fixation_matrix(allele_counts_map, passed_sites_map, allowed_genes=core_genes, min_change=min_change)    
+    chunk_snp_difference_matrix, chunk_snp_opportunity_matrix = diversity_utils.calculate_fixation_matrix(allele_counts_map, passed_sites_map, allowed_genes=core_genes, min_change=min_change)
     sys.stderr.write("Done!\n")
-    
+
     if snp_difference_matrix.shape[0]==0:
         snp_difference_matrix = numpy.zeros_like(chunk_snp_difference_matrix)*1.0
         snp_opportunity_matrix = numpy.zeros_like(snp_difference_matrix)*1.0
@@ -233,60 +233,60 @@ while final_line_number >= 0:
         nonsynonymous_opportunity_matrix = numpy.zeros_like(snp_difference_matrix)
 
         n = len(snp_samples)
-        
+
         maf_bins = numpy.arange(1,n+1)*1.0/n
         maf_bins -= (maf_bins[1]-maf_bins[0])/2
         maf_bins[0]=-0.1
         maf_bins[-1] = 1.1
         mafs = numpy.arange(1,n)*1.0/n
-    
+
         count_bins = numpy.arange(1,n+1)-0.5
-        count_locations = numpy.arange(1,n)    
-    
+        count_locations = numpy.arange(1,n)
+
         synonymous_sfs = numpy.zeros_like(mafs)
         nonsynonymous_sfs = numpy.zeros_like(mafs)
 
         synonymous_count_sfs = numpy.zeros_like(count_locations)
         nonsynonymous_count_sfs = numpy.zeros_like(count_locations)
 
-    
+
     snp_difference_matrix += chunk_snp_difference_matrix
     snp_opportunity_matrix += chunk_snp_opportunity_matrix
 
     # Calculate fixation matrix
     sys.stderr.write("Calculating matrix of 4D differences...\n")
-    chunk_synonymous_difference_matrix, chunk_synonymous_opportunity_matrix =    diversity_utils.calculate_fixation_matrix(allele_counts_map, passed_sites_map, allowed_genes=core_genes, min_change=min_change,allowed_variant_types=set(['4D']))    
+    chunk_synonymous_difference_matrix, chunk_synonymous_opportunity_matrix =    diversity_utils.calculate_fixation_matrix(allele_counts_map, passed_sites_map, allowed_genes=core_genes, min_change=min_change,allowed_variant_types=set(['4D']))
     sys.stderr.write("Done!\n")
-    
+
     synonymous_difference_matrix += chunk_synonymous_difference_matrix
     synonymous_opportunity_matrix += chunk_synonymous_opportunity_matrix
 
     # Calculate fixation matrix
     sys.stderr.write("Calculating matrix of 1D differences...\n")
-    chunk_nonsynonymous_difference_matrix, chunk_nonsynonymous_opportunity_matrix =    diversity_utils.calculate_fixation_matrix(allele_counts_map, passed_sites_map, allowed_genes=core_genes, min_change=min_change,allowed_variant_types=set(['1D']))    
+    chunk_nonsynonymous_difference_matrix, chunk_nonsynonymous_opportunity_matrix =    diversity_utils.calculate_fixation_matrix(allele_counts_map, passed_sites_map, allowed_genes=core_genes, min_change=min_change,allowed_variant_types=set(['1D']))
     sys.stderr.write("Done!\n")
-    
+
     nonsynonymous_difference_matrix += chunk_nonsynonymous_difference_matrix
     nonsynonymous_opportunity_matrix += chunk_nonsynonymous_opportunity_matrix
-  
+
     sys.stderr.write("Calculating the SFS...\n")
     chunk_synonymous_freqs = diversity_utils.calculate_pooled_freqs(allele_counts_map, passed_sites_map, allowed_variant_types = set(['4D']), allowed_genes=core_genes)
     chunk_nonsynonymous_freqs = diversity_utils.calculate_pooled_freqs(allele_counts_map, passed_sites_map, allowed_variant_types = set(['1D']), allowed_genes=core_genes)
-        
-    chunk_synonymous_sfs, dummy = numpy.histogram(chunk_synonymous_freqs, bins=maf_bins) 
+
+    chunk_synonymous_sfs, dummy = numpy.histogram(chunk_synonymous_freqs, bins=maf_bins)
     synonymous_sfs += chunk_synonymous_sfs
-        
-    chunk_nonsynonymous_sfs, dummy = numpy.histogram(chunk_nonsynonymous_freqs, bins=maf_bins) 
+
+    chunk_nonsynonymous_sfs, dummy = numpy.histogram(chunk_nonsynonymous_freqs, bins=maf_bins)
     nonsynonymous_sfs += chunk_nonsynonymous_sfs
-    
+
     sys.stderr.write("Calculating count SFS...\n")
     chunk_synonymous_counts, chunk_synonymous_weights = diversity_utils.calculate_pooled_counts(allele_counts_map, passed_sites_map, allowed_variant_types = set(['4D']), allowed_genes=core_genes,pi_min_k=4)
     chunk_nonsynonymous_counts, chunk_nonsynonymous_weights = diversity_utils.calculate_pooled_counts(allele_counts_map, passed_sites_map, allowed_variant_types = set(['1D']), allowed_genes=core_genes,pi_min_k=4)
-        
-    chunk_synonymous_count_sfs, dummy = numpy.histogram(chunk_synonymous_counts, bins=count_bins) 
+
+    chunk_synonymous_count_sfs, dummy = numpy.histogram(chunk_synonymous_counts, bins=count_bins)
     synonymous_count_sfs += chunk_synonymous_count_sfs
-        
-    chunk_nonsynonymous_count_sfs, dummy = numpy.histogram(chunk_nonsynonymous_counts, bins=count_bins) 
+
+    chunk_nonsynonymous_count_sfs, dummy = numpy.histogram(chunk_nonsynonymous_counts, bins=count_bins)
     nonsynonymous_count_sfs += chunk_nonsynonymous_count_sfs
 
     synonymous_pi_weighted_counts += chunk_synonymous_weights
@@ -297,16 +297,16 @@ largest_clade_matrix_idxs_i = []
 largest_clade_matrix_idxs_j = []
 for i in xrange(0, snp_difference_matrix.shape[0]):
     for j in xrange(i+1, snp_difference_matrix.shape[0]):
-        
+
         largest_clade_matrix_idxs_i.append(i)
         largest_clade_matrix_idxs_j.append(j)
 
 largest_clade_matrix_idxs = largest_clade_matrix_idxs_i, largest_clade_matrix_idxs_j
-    
-    
-opportunity_ratio = nonsynonymous_opportunity_matrix[largest_clade_matrix_idxs].sum()*1.0 / synonymous_opportunity_matrix[largest_clade_matrix_idxs].sum() 
 
-pi_ratio = nonsynonymous_difference_matrix[largest_clade_matrix_idxs].sum()*1.0 / synonymous_difference_matrix[largest_clade_matrix_idxs].sum() 
+
+opportunity_ratio = nonsynonymous_opportunity_matrix[largest_clade_matrix_idxs].sum()*1.0 / synonymous_opportunity_matrix[largest_clade_matrix_idxs].sum()
+
+pi_ratio = nonsynonymous_difference_matrix[largest_clade_matrix_idxs].sum()*1.0 / synonymous_difference_matrix[largest_clade_matrix_idxs].sum()
 
 piNpiS = pi_ratio/opportunity_ratio
 
@@ -320,6 +320,12 @@ sys.stderr.write("%d rest-tons!\n" % (synonymous_count_sfs[3:]+nonsynonymous_cou
 
 sys.stderr.write("%g pi-weighted!\n" % (nonsynonymous_pi_weighted_counts+synonymous_pi_weighted_counts))
 
+output_sfs = '../../Data/' + species_name + '.sfs'
+output_figure = '../../Data/' + species_name + '_sfs.pdf'
+with open(output_sfs, 'w') as f:
+    f.write('The count locations are: ' + str(count_locations)[1:-1])
+    f.write('The synonymous SFS is: ' + str(synonymous_count_sfs)[1:-1])
+    f.write('The nonsynonymous SFS is: ' + str(nonsynonymous_count_sfs)[1:-1])
 
 print count_locations
 print synonymous_count_sfs
@@ -352,5 +358,5 @@ count_axis.bar([3-0.3],[pi_weighted_fraction],width=0.6,color='r',linewidth=0)
 
 sys.stderr.write("Saving figure...\t")
 fig.savefig('%s/figure_5%s.pdf' % (parse_midas_data.analysis_directory, other_species_str),bbox_inches='tight')
+fig.savefig(output_figure, bbox_inches='tight')
 sys.stderr.write("Done!\n")
-
