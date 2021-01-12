67ec275319bebc1c8a78d3061d9d373de5ac11b7
jon-mah <jonmah@uw.edu>
2021-01-12 15:04:54 -0800

    figure output.

--- microbiome_evolution-master/plot_within_clade_sfs.py
+++ microbiome_evolution-master/plot_within_clade_sfs.py
@@ -44,8 +44,8 @@ mpl.rcParams['lines.linewidth'] = 0.5
 mpl.rcParams['legend.frameon']  = False
 mpl.rcParams['legend.fontsize']  = 'small'
 
-# species_name = "Bacteroides_vulgatus_57955"
-species_name = "Bacteroides_uniformis_57318"
+species_name = "Bacteroides_vulgatus_57955"
+# species_name = "Bacteroides_uniformis_57318"
 # species_name = "Alistipes_putredinis_61533"
 # species_name = "Bacteroides_ovatus_58035"
 # species_name = "Eubacterium_rectale_56927"
