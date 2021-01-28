ff759f09d195fc7979c69653819c15d5697bf640
Jonathan Mah <jonmah@uw.edu>
2021-01-28 11:39:09 -0800

    inference scripts for high recombination species.

---
+++ Scripts/p_copri_inference.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+#$ -cwd
+#$ -V
+#$ -m ea
+#$ -l h_data=5G
+#$ -l h_rt=02:00:00
+
+# This script infers the demography of a given example synonymous sfs.
+
+python fit_demographic_model.py ../Data/Prevotella_copri_61740_syn.sfs ../Analysis/Prevotella_copri_61740/
