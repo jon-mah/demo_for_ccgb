1583bbce33aaedc1cee36ad84001a8e89e6f5f5d
jon-mah <jonmah@uw.edu>
2022-01-24 11:16:29 -0800

    file cleanup

--- Scripts/MIDAS_snps.bash
+++ Scripts/MIDAS_snps.bash
@@ -2,12 +2,12 @@
 #$ -N MIDAS_species.bash
 #$ -cwd # Run qsub script from desired working directory
 #$ -V
-#$ -e /u/home/j/jonmah/postproc_error
-#$ -o /u/home/j/jonmah/postproc_output
-#$ -l h_data=15G
-#$ -l time=72:00:00
+#$ -l h_data=12G
+#$ -l time=47:00:00
 #$ -l highp
 #$ -t 1-339
+
+# 182, 276
 
 # module load python/2.7
 export PYTHONPATH=$PYTHONPATH:/u/project/ngarud/Garud_lab/MIDAS
@@ -29,8 +29,6 @@ OUTDIR=../Data/oral_microbiome_data/${file}/
 file_1=${OUTDIR}${file}.denovo_duplicates_marked.trimmed.1.fastq.gz
 file_2=${OUTDIR}${file}.denovo_duplicates_marked.trimmed.2.fastq.gz
 
-# module load singularity
-
 # echo ${file_1}
 # echo ${file_2}
 # echo $OUTDIR
@@ -41,5 +39,7 @@ echo "File is found"
 else
    echo "File is not found"
    singularity exec $H2_CONTAINER_LOC/MIDAS-mod.sif run_midas.py species $OUTDIR -1 ${file_1} -2 ${file_2}
-   # echo $SGE_TASK_ID >> species_errors.txt
 fi
+
+# module load singularity
+# singularity exec $H2_CONTAINER_LOC/MIDAS-mod.sif run_midas.py species $OUTDIR -1 ${file_1} -2 ${file_2}
