8496140f26886b749edbe15511d7e7208ece8d50
jon-mah <jonmah@uw.edu>
2022-06-03 16:17:12 -0700

    downsample script.

--- Scripts/compute_downsampled_sfs.bash
+++ Scripts/compute_downsampled_sfs.bash
@@ -6,7 +6,8 @@
 #$ -l h_rt=02:00:00
 
 # This script infers the demography of a given example synonymous sfs.
-
-species=Veillonella_sp_62404
+# species=Veillonella_sp_62404
+# species=Faecalibacterium_prausnitzii_61481
+species=Bacteriudes_uniformis
 
 python compute_downsampled_sfs.py ${species} ../Analysis/${species}
--- Scripts/compute_downsampled_sfs.py
+++ Scripts/compute_downsampled_sfs.py
@@ -1,7 +1,7 @@
 """
 Summarizes the average pi values per sample across species and populations.
 
-JCM 20211118
+JCM 20220516
 """
 
 
@@ -75,13 +75,13 @@ class ComputeDownSampledSFS():
                 args['outprefix'], underscore, species)
 
         # Load core genes
-        core_genes = parse_midas_data.load_core_genes(species)
+        core_genes = pmd.load_core_genes(species)
 
         # Load allele counts map for this species
-        snp_samples, allele_counts_map, passed_sites_map, final_line_number = parse_midas_data.parse_snps(species_name, allowed_variant_types=['1D','2D','3D','4D'], allowed_genes=core_genes)
+        snp_samples, allele_counts_map, passed_sites_map, final_line_number = pmd.parse_snps(species, allowed_variant_types=['1D','2D','3D','4D'], allowed_genes=core_genes)
 
         # Pooled counts: for reference
-        synonymous_counts, synonymous_weights = diversity_utils.calculate_pooled_counts(allele_counts_map, passed_sites_map, allowed_variant_types = set(['4D']), allowed_genes=core_genes,pi_min_k=4)
+        # synonymous_counts, synonymous_weights = diversity_utils.calculate_pooled_counts(allele_counts_map, passed_sites_map, allowed_variant_types = set(['4D']), allowed_genes=core_genes,pi_min_k=4)
 
         # Dive into internals of calculate_pooled_counts
         allowed_variant_types = '4D'
@@ -90,30 +90,33 @@ class ComputeDownSampledSFS():
         lower_threshold = 0.2
         upper_threshold = 0.8
 
+        print(allowed_genes)
+        # print(allele_counts_map)
+
         for gene_name in allowed_genes:
-          for variant_type in allele_counts_map[gene_name].keys():
-            if variant_type not in allowed_variant_types:
-                continue
+            for variant_type in allele_counts_map[gene_name].keys():
+                if variant_type not in allowed_variant_types:
+                    continue
 
-            allele_counts = allele_counts_map[gene_name][variant_type]['alleles']
+                allele_counts = allele_counts_map[gene_name][variant_type]['alleles']
 
-            if len(allele_counts)==0:
-                continue
+                if len(allele_counts)==0:
+                    continue
 
-            allele_counts = allele_counts
+                allele_counts = allele_counts
 
-            genotype_matrix, passed_sites_matrix = diversity_utils.calculate_consensus_genotypes(allele_counts,lower_threshold,upper_threshold)
-            prevalences = (genotype_matrix*passed_sites_matrix).sum(axis=1)
-            min_prevalences = 0.5
-            max_prevalences = (passed_sites_matrix).sum(axis=1)-0.5
+                genotype_matrix, passed_sites_matrix = diversity_utils.calculate_consensus_genotypes(allele_counts,lower_threshold,upper_threshold)
+                prevalences = (genotype_matrix*passed_sites_matrix).sum(axis=1)
+                min_prevalences = 0.5
+                max_prevalences = (passed_sites_matrix).sum(axis=1)-0.5
 
-            polymorphic_sites = (prevalences>min_prevalences)*(prevalences<max_prevalences)
+                polymorphic_sites = (prevalences>min_prevalences)*(prevalences<max_prevalences)
 
-            ks = prevalences[polymorphic_sites]
-            ns = passed_sites_matrix.sum(axis=1)[polymorphic_sites]
-            minor_ks = numpy.fmin(ks,ns-ks)
+                ks = prevalences[polymorphic_sites]
+                ns = passed_sites_matrix.sum(axis=1)[polymorphic_sites]
+                minor_ks = numpy.fmin(ks,ns-ks)
 
-            break
+                break
 
         # Finally, do whatever you need to do to figure out what the data structure and data is
         print(allele_counts)
