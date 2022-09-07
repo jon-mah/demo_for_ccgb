1e707d644018896ee4cf60780fa33dcba4a0eb68
jon-mah <jonmah@uw.edu>
2022-09-07 15:34:13 -0700

    plot likelihood test.

--- Scripts/plot_likelihood.py
+++ Scripts/plot_likelihood.py
@@ -439,10 +439,10 @@ class PlotLikelihood():
                 logger.info('Beginning demographic inference for two-epoch '
                             'demographic model.')
             with open(file, 'w') as f:
-                # min_nu = 0.1 * nu_prime
-                # max_nu = 1.9 * nu_prime
                 min_nu = 0.1 * nu_prime
-                max_nu = 2.0
+                max_nu = 1.9 * nu_prime
+                # min_nu = 0.1 * nu_prime
+                # max_nu = 2.0
                 min_tau = 0.1 * tau_prime
                 max_tau = 1.9 * tau_prime
                 nx = 20
