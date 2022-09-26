2ed45c72f2e7058097af960d377f523966b2ac76
jon-mah <jonmah@uw.edu>
2022-09-26 10:35:32 -0700

    plot likelihood outputs

--- Scripts/plot_likelihood.py
+++ Scripts/plot_likelihood.py
@@ -443,8 +443,8 @@ class PlotLikelihood():
                 max_nu = 1.9 * nu_prime
                 min_tau = 0.1 * tau_prime
                 max_tau = 10 * tau_prime
-                nx = 5
-                ny = 5
+                nx = 15
+                ny = 15
                 x_space = numpy.linspace(min_nu, max_nu, nx)
                 y_space = numpy.logspace(numpy.log10(min_tau), numpy.log10(max_tau), ny, base=10)
                 x, y = numpy.meshgrid(x_space, y_space,
