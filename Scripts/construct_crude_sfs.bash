2ddaffba1dd9b2af255257a7b766f79cf683ddf0
jon-mah <jonmah@g.ucla.edu>
2022-10-19 16:56:37 -0700

    construct_crude_sfs bugfix.

--- Scripts/construct_crude_sfs.bash
+++ Scripts/construct_crude_sfs.bash
@@ -1,9 +1,11 @@
 #!/bin/bash
 #$ -N construct_crude_sfs.bash
-#$ -cwd # Run qsub script from desired working directory
+#$ -cwd
+#$ -V
+#$ -e /u/home/j/jonmah/postproc_error
+#$ -o /u/home/j/jonmah/postproc_output
 #$ -l highp
 #$ -l h_data=25G
 #$ -l time=00:05:00
-#$ -m be
 
 python construct_crude_sfs.py ../Data/oral_microbiome_data/merged_data/snps/ Veillonell_parvul_57794 --min_depth 2 ../Data/oral_microbiome_data/Veillonella_parvula_57794_sfs
