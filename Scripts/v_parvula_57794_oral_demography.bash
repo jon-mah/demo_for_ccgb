fd4e4d582534a4a28f65c24c3da077bd220f1838
jon-mah <jonmah@uw.edu>
2022-10-20 13:18:12 -0700

    oral SFS and one epoch model fit.

--- Scripts/v_parvula_57794_oral_demography.bash
+++ Scripts/v_parvula_57794_oral_demography.bash
@@ -6,7 +6,7 @@
 #$ -l highp
 #$ -l h_rt=00:10:00
 
-python fit_one_epoch.py ../Data/oral_microbiome_data/Veillonella_parvula_57794_sfs/consensus_sfs_2.txt ../Data/oral_microbiome_data/Veillonella_parvula_57794_sfs/2/
-python fit_one_epoch.py ../Data/oral_microbiome_data/Veillonella_parvula_57794_sfs/consensus_sfs_3.txt ../Data/oral_microbiome_data/Veillonella_parvula_57794_sfs/3/
-python fit_one_epoch.py ../Data/oral_microbiome_data/Veillonella_parvula_57794_sfs/consensus_sfs_5.txt ../Data/oral_microbiome_data/Veillonella_parvula_57794_sfs/5/
-python fit_one_epoch.py ../Data/oral_microbiome_data/Veillonella_parvula_57794_sfs/consensus_sfs_25.txt ../Data/oral_microbiome_data/Veillonella_parvula_57794_sfs/25/
+python fit_one_epoch.py ../Data/oral_microbiome_data/Veillonella_parvula_57794_sfs/folded_sfs_2.txt ../Data/oral_microbiome_data/Veillonella_parvula_57794_sfs/2/
+python fit_one_epoch.py ../Data/oral_microbiome_data/Veillonella_parvula_57794_sfs/folded_sfs_3.txt ../Data/oral_microbiome_data/Veillonella_parvula_57794_sfs/3/
+python fit_one_epoch.py ../Data/oral_microbiome_data/Veillonella_parvula_57794_sfs/folded_sfs_5.txt ../Data/oral_microbiome_data/Veillonella_parvula_57794_sfs/5/
+python fit_one_epoch.py ../Data/oral_microbiome_data/Veillonella_parvula_57794_sfs/folded_sfs_25.txt ../Data/oral_microbiome_data/Veillonella_parvula_57794_sfs/25/
