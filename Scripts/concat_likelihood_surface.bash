8d6670a31062d1c056192ed583272dc1d6c28f9f
jon-mah <jonmah@g.ucla.edu>
2022-11-01 11:17:53 -0700

    concat contractions.

---
+++ Scripts/concat_likelihood_surface.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+#$ -cwd
+#$ -V
+#$ -m ea
+#$ -l h_data=5G
+#$ -l h_rt=00:10:00
+#$ -t 1-30
+
+i=0
+while read line;
+do
+  i=$((i+1))
+  if [ $i -eq $SGE_TASK_ID ]
+    then
+      cat ../Analysis/gut_contraction_surfaces/${line} - > ../Analysis/gut_likelihood_surfaces/${line} ;
+  fi
+done < ../Analysis/temp_file_list.txt
