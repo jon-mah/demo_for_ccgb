5a9ae4f75cc0b26a56dcd48b530621431519ecee
jon-mah <jonmah@g.ucla.edu>
2022-11-03 12:45:20 -0700

    gut consensus sfs script.

--- Scripts/construct_crude_sfs.bash
+++ Scripts/construct_crude_sfs.bash
@@ -11,15 +11,27 @@
 
 # SGE_TASK_ID=1
 
+# i=0
+# while read line;
+#  do
+#    i=$((i+1))
+#    # echo $line
+#    if [ $i -eq $SGE_TASK_ID ]
+#    then
+#       species=$line
+#    fi
+# done < oral_species_list.txt
+
 i=0
 while read line;
-do
-   i=$((i+1))
-   # echo $line
-   if [ $i -eq $SGE_TASK_ID ]
-   then
+  do
+    i=$((i+1))
+    # echo $line
+    if [ $i -eq $SGE_TASK_ID ]
+    then
       species=$line
-   fi
-done < oral_species_list.txt
+    fi
+done < gut_sfs_list.txt
 
-python construct_crude_sfs.py ../Data/oral_microbiome_data/merged_data/snps/ $species --min_depth 10 ../Data/oral_microbiome_sfs/${species}_sfs/
+# python construct_crude_sfs.py ../Data/oral_microbiome_data/merged_data/snps/ $species --min_depth 10 ../Data/oral_microbiome_sfs/${species}_sfs/
+python construct_crude_sfs.py ../Data/microbiome_data/merged_data/snps/ $species --min_depth 10 ../Data/gut_microbiome_sfs/${species}_sfs/
