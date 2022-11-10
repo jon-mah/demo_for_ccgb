4456a2c33ec9a9d8aaeabe46b9a2db3f600b3ea9
jon-mah <jonmah@uw.edu>
2022-11-10 13:00:50 -0800

    merge

---
+++ Data/streptococcus_mutans_isolates/SRA_accessions/fastq_dump.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+#$ -cwd
+#$ -V
+#$ -e /u/home/j/jonmah/postproc_error
+#$ -o /u/home/j/jonmah/postproc_output
+#$ -l h_data=25G
+#$ -l h_rt=2:00:00
+#$ -l highp
+#$ -t 1-22
+
+i=0
+while read line;
+do
+   i=$((i+1))
+   # echo $line
+   if [ $i -eq $SGE_TASK_ID ]
+   then
+      file=$line
+   fi
+done < ../SraAccList.txt
+
+# mkdir $file
+fasterq-dump $file -O ./${file}
---
+++ Data/streptococcus_mutans_isolates/gzip.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+#$ -N edirect
+#$ -cwd # Run qsub script from desired working directory
+#$ -V # Same environment
+#$ -e /u/home/j/jonmah/postproc_error
+#$ -o /u/home/j/jonmah/postproc_output
+#$ -l h_data=25G
+#$ -l time=4:00:00
+#$ -l highp
+#$ -t 1-48
+
+# 48 lines
+
+i=0
+while read line;
+do
+   i=$((i+1))
+   # echo $line
+   if [ $i -eq $SGE_TASK_ID ]
+   then
+      file=$line
+   fi
+done < ../streptococcus_mutans_genbank_accessions.txt
+
+# esearch -db nucleotide -query "${file}" | efetch -format fasta > fastq_MIDAS_intermediate/${file}.fasta
+gzip fastq_MIDAS_intermediate/${file}.fasta
