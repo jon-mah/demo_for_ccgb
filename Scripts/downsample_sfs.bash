02c14d303e20ec36f5b8543ba0771207ce27b774
jon-mah <jonmah@g.ucla.edu>
2023-03-23 13:18:55 -0700

    downsampling bash script.

--- Scripts/downsample_sfs.bash
+++ Scripts/downsample_sfs.bash
@@ -7,19 +7,7 @@
 #$ -l h_data=25G
 #$ -l h_rt=00:30:00
 
-python downsample_sfs.py ../Data/UHGG/UHGG_Bacteroides_fragilis/full_output_sfs.txt 14 ../Data/UHGG/UHGG_Bacteroides_fragilis/downsampled_14_sfs.txt
-python downsample_sfs.py ../Data/UHGG/UHGG_Bacteroides_stercoris/full_output_sfs.txt 14 ../Data/UHGG/UHGG_Bacteroides_stercoris/downsampled_14_sfs.txt
-python downsample_sfs.py ../Data/UHGG/UHGG_Faecalibacterium_prausnitzii_K/full_output_sfs.txt 14 ../Data/UHGG/UHGG_Faecalibacterium_prausnitzii_K/downsampled_14_sfs.txt
-python downsample_sfs.py ../Data/UHGG/UHGG_Prevotella_copri/full_output_sfs.txt 14 ../Data/UHGG/UHGG_Prevotella_copri/downsampled_14_sfs.txt
-# python downsample_sfs.py ../Data/UHGG/UHGG_Bacteroides_fragilis/full_output_sfs.txt 14 ../Data/UHGG/UHGG_Bacteroides_fragilis/downsampled_14_sfs.txt
-# python downsample_sfs.py ../Data/UHGG/UHGG_Bacteroides_fragilis/full_output_sfs.txt 14 ../Data/UHGG/UHGG_Bacteroides_fragilis/downsampled_14_sfs.txt
-# python downsample_sfs.py ../Data/UHGG/UHGG_Bacteroides_fragilis/full_output_sfs.txt 14 ../Data/UHGG/UHGG_Bacteroides_fragilis/downsampled_14_sfs.txt
-# python downsample_sfs.py ../Data/UHGG/UHGG_Bacteroides_fragilis/full_output_sfs.txt 14 ../Data/UHGG/UHGG_Bacteroides_fragilis/downsampled_14_sfs.txt
-# python downsample_sfs.py ../Data/UHGG/UHGG_Bacteroides_fragilis/full_output_sfs.txt 14 ../Data/UHGG/UHGG_Bacteroides_fragilis/downsampled_14_sfs.txt
-# python downsample_sfs.py ../Data/UHGG/UHGG_Bacteroides_fragilis/full_output_sfs.txt 14 ../Data/UHGG/UHGG_Bacteroides_fragilis/downsampled_14_sfs.txt
-# python downsample_sfs.py ../Data/UHGG/UHGG_Bacteroides_fragilis/full_output_sfs.txt 14 ../Data/UHGG/UHGG_Bacteroides_fragilis/downsampled_14_sfs.txt
-# python downsample_sfs.py ../Data/UHGG/UHGG_Bacteroides_fragilis/full_output_sfs.txt 14 ../Data/UHGG/UHGG_Bacteroides_fragilis/downsampled_14_sfs.txt
-# python downsample_sfs.py ../Data/UHGG/UHGG_Bacteroides_fragilis/full_output_sfs.txt 14 ../Data/UHGG/UHGG_Bacteroides_fragilis/downsampled_14_sfs.txt
-# python downsample_sfs.py ../Data/UHGG/UHGG_Bacteroides_fragilis/full_output_sfs.txt 14 ../Data/UHGG/UHGG_Bacteroides_fragilis/downsampled_14_sfs.txt
-# python downsample_sfs.py ../Data/UHGG/UHGG_Bacteroides_fragilis/full_output_sfs.txt 14 ../Data/UHGG/UHGG_Bacteroides_fragilis/downsampled_14_sfs.txt
-# python downsample_sfs.py ../Data/UHGG/UHGG_Bacteroides_fragilis/full_output_sfs.txt 14 ../Data/UHGG/UHGG_Bacteroides_fragilis/downsampled_14_sfs.txt
+python downsample_sfs.py ../Data/UHGG/UHGG_Bacteroides_fragilis/full_output_sfs.txt 14 ../Data/UHGG/UHGG_Bacteroides_fragilis/
+python downsample_sfs.py ../Data/UHGG/UHGG_Bacteroides_stercoris/full_output_sfs.txt 14 ../Data/UHGG/UHGG_Bacteroides_stercoris/
+python downsample_sfs.py ../Data/UHGG/UHGG_Faecalibacterium_prausnitzii_K/full_output_sfs.txt 14 ../Data/UHGG/UHGG_Faecalibacterium_prausnitzii_K/
+python downsample_sfs.py ../Data/UHGG/UHGG_Prevotella_copri/full_output_sfs.txt 14 ../Data/UHGG/UHGG_Prevotella_copri/
