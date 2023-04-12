ba01c3216fba0fa28d87a14da259192fd47e58a3
jon-mah <jonmah@uw.edu>
2023-04-12 14:58:48 -0700

    Complete SFS demography.

--- Scripts/fit_one_epoch.py
+++ Scripts/fit_one_epoch.py
@@ -417,7 +417,7 @@ class DemographicInference():
         # model_list = ['exponential_growth', 'two_epoch', 'bottleneck_growth',
         #               'three_epoch', 'one_epoch']
         # model_list = ['two_epoch', 'one_epoch']
-        model_list = ['three_epoch']
+        model_list = ['one_epoch']
         # Fit different one-epoch models and compute likelihood
         method_list = ['expectation']
         for method in method_list:
