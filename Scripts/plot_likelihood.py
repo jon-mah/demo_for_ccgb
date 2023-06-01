59b857282191b47ad8b12309f2e283b43d6bf066
jon-mah <jonmah@uw.edu>
2023-06-01 14:56:03 -0700

    B. cellulosilyticus likeliehood

--- Scripts/plot_likelihood.py
+++ Scripts/plot_likelihood.py
@@ -466,8 +466,8 @@ class PlotLikelihood():
 
                 npts = 25
 
-                x_range = numpy.linspace(x * 0.75, x * 1.25, npts)
-                y_range = numpy.linspace(y * 0.9, y * 1.1, npts)
+                x_range = numpy.linspace(x * 0.8, x * 1.2, npts)
+                y_range = numpy.linspace(y * 0.8, y * 1.2, npts)
 
                 X, Y = numpy.meshgrid(x_range, y_range)
 
