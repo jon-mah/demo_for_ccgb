24db2902d265f4a236aba4aa8105f682f841d376
jon-mah <jonmah@uw.edu>
2023-06-05 16:58:10 -0700

    E. eligens

--- Scripts/plot_likelihood.py
+++ Scripts/plot_likelihood.py
@@ -471,8 +471,8 @@ class PlotLikelihood():
                 # y_range = numpy.linspace(y  * 0.9, y * 1.3, npts)
 
                 # E. eligens
-                x_range = numpy.linspace(x * 0.6, x * 1.4, npts)
-                y_range = numpy.linspace(y * 0.6, y * 1.4, npts)
+                x_range = numpy.linspace(x * 0.7, x * 1.3, npts)
+                y_range = numpy.linspace(y * 0.7, y * 1.3, npts)
                 
 
                 X, Y = numpy.meshgrid(x_range, y_range)
