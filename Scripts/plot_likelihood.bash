12eb7763f97b97682ee70786eb3001d04c5837bc
jon-mah <jonmah@uw.edu>
2023-07-21 09:09:20 -0700

    Likelihood surfaces

--- Scripts/plot_likelihood.bash
+++ Scripts/plot_likelihood.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 #$ -cwd
 #$ -V
-#$ -N plot_likelihood.bash
+#$ -N accessory_likelihood
 #$ -e /u/home/j/jonmah/postproc_error
 #$ -o /u/home/j/jonmah/postproc_output
 #$ -l h_data=15G
@@ -9,7 +9,7 @@
 #$ -l highp
 #$ -t 1-30
 
-# SGE_TASK_ID=8
+# SGE_TASK_ID=1
 
 # Complete SFS
 i=0
@@ -22,4 +22,5 @@ while read line;
     fi
 done < ../Data/good_species_list.txt
 
-python plot_likelihood.py ../Analysis/${species}_downsampled_14/core_empirical_syn_downsampled_sfs.txt ../Analysis/${species}_downsampled_14/core_two_epoch_demography.txt ../Analysis/${species}_downsampled_14/
+# python plot_likelihood.py ../Analysis/${species}_downsampled_14/core_empirical_syn_downsampled_sfs.txt ../Analysis/${species}_downsampled_14/core_two_epoch_demography.txt ../Analysis/${species}_downsampled_14/core
+python plot_likelihood.py ../Analysis/${species}_downsampled_14/accessory_empirical_syn_downsampled_sfs.txt ../Analysis/${species}_downsampled_14/accessory_two_epoch_demography.txt ../Analysis/${species}_downsampled_14/accessory
