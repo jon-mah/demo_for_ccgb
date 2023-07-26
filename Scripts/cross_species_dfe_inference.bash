73bcc5147a40224246dcead777dab9fa4fe97497
jon-mah <jonmah@uw.edu>
2023-07-26 16:51:14 -0700

    Cross DFE, inferred DFE

--- Scripts/cross_species_dfe_inference.bash
+++ Scripts/cross_species_dfe_inference.bash
@@ -5,12 +5,12 @@
 #$ -l h_data=25G
 #$ -l h_rt=48:00:00
 #$ -l highp
-#$ -t 28
+#$ -t 26-30
 #$ -e /u/home/j/jonmah/postproc_error
 #$ -o /u/home/j/jonmah/postproc_output
 #$ -N cross_species_inference
 
-# SGE_TASK_ID=1
+# SGE_TASK_ID=25
 
 i=0
 while read line;
@@ -22,4 +22,5 @@ do
   fi
 done < ../Data/good_species_list.txt
 
-python cross_species_dfe_inference.py ../Analysis/${species}_downsampled_14/complete_two_epoch_demography.txt ../Analysis/${species}_downsampled_14/empirical_nonsyn_downsampled_sfs.txt ../Analysis/cross_species_dfe/
+python cross_species_dfe_inference.py ../Analysis/${species}_downsampled_14/core_two_epoch_demography.txt ../Analysis/${species}_downsampled_14/core_empirical_nonsyn_downsampled_sfs.txt ../Analysis/cross_species_dfe/
+# python cross_species_dfe_inference.py ../Analysis/${species}_downsampled_14/core_three_epoch_demography.txt ../Analysis/${species}_downsampled_14/core_empirical_nonsyn_downsampled_sfs.txt ../Analysis/cross_species_dfe/
