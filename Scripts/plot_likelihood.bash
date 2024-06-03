cd40c25f90bcca124ece09dc6e514a6d6839cd61
jon-mah <jonmah@g.ucla.edu>
2024-06-03 07:00:58 -0700

    Plot likelihood

--- Scripts/plot_likelihood.bash
+++ Scripts/plot_likelihood.bash
@@ -1,15 +1,25 @@
 #!/bin/bash
 #$ -cwd
 #$ -V
-#$ -N accessory_likelihood
+#$ -N high_recombination_likelihood
 #$ -e /u/home/j/jonmah/postproc_error
 #$ -o /u/home/j/jonmah/postproc_output
-#$ -l h_data=15G
-#$ -l h_rt=12:00:00
+#$ -l h_rt=01:00:00
 #$ -l highp
-#$ -t 29
+#$ -t 1-22
 
 # SGE_TASK_ID=1
+
+# Complete SFS
+# i=0
+# while read line;
+#   do
+#     i=$((i+1))
+#     # echo $line
+#     if [ $i -eq $SGE_TASK_ID ]; then
+#         species=$line
+#     fi
+# done < ../Data/good_species_list.txt
 
 # Complete SFS
 i=0
@@ -20,7 +30,8 @@ while read line;
     if [ $i -eq $SGE_TASK_ID ]; then
         species=$line
     fi
-done < ../Data/good_species_list.txt
+done < ../HighRecombinationData/good_species_list.txt
 
-python plot_likelihood.py ../Analysis/${species}_downsampled_14/core_empirical_syn_downsampled_sfs.txt ../Analysis/${species}_downsampled_14/core_two_epoch_demography.txt ../Analysis/${species}_downsampled_14/core
+# python plot_likelihood.py ../Analysis/${species}_downsampled_14/core_empirical_syn_downsampled_sfs.txt ../Analysis/${species}_downsampled_14/core_two_epoch_demography.txt ../Analysis/${species}_downsampled_14/core
 # python plot_likelihood.py ../Analysis/${species}_downsampled_14/accessory_empirical_syn_downsampled_sfs.txt ../Analysis/${species}_downsampled_14/accessory_two_epoch_demography.txt ../Analysis/${species}_downsampled_14/accessory
+python plot_likelihood.py ../HighRecombinationAnalysis/${species}/core_0.5_empirical_syn_14_downsampled_sfs.txt ../HighRecombinationAnalysis/${species}/core_0.05_two_epoch_demography.txt ../HighRecombinationAnalysis/${species}/
