2f8aa74dc61a9a5d501838b6715f5045739aac43
jon-mah <jonmah@uw.edu>
2024-06-10 17:20:20 -0700

    compute_sfs.bash

--- Scripts/compute_sfs.bash
+++ Scripts/compute_sfs.bash
@@ -5,18 +5,19 @@
 #$ -cwd
 #$ -V
 #$ -l h_data=25G
-#$ -l h_rt=11:50:00
+#$ -l h_rt=2:00:00
 #$ -l highp
-#$ -t 1:72
+#$ -t 1:128
 
-SGE_TASK_ID=2
+# SGE_TASK_ID=2
 
 # readarray good_spec < ../Data/good_species_list.txt
-readarray good_spec < ../HighRecombinationData/good_species_list.txt
-
+# readarray good_spec < ../HighRecombinationData/good_species_list.txt
+readarray good_spec < ../SupplementaryAnalysis/supplementary_species_list.txt
 good_spec=( null ${good_spec[@]} )
 
 # python compute_sfs.py ${good_spec[$SGE_TASK_ID]} ../Analysis/${good_spec[$SGE_TASK_ID]}/
-python compute_sfs.py ${good_spec[$SGE_TASK_ID]} ../HighRecombinationAnalysis/${good_spec[$SGE_TASK_ID]}/ --core
+# python compute_sfs.py ${good_spec[$SGE_TASK_ID]} ../HighRecombinationAnalysis/${good_spec[$SGE_TASK_ID]}/ --core
 # python compute_sfs.py ${good_spec[$SGE_TASK_ID]} ../Analysis/${good_spec[$SGE_TASK_ID]}/ --accessory
 # echo ${good_spec[SGE_TASK_ID]}
+python compute_sfs.py ${good_spec[$SGE_TASK_ID]} ../SupplementaryAnalysis/${good_spec[$SGE_TASK_ID]}/ --core
