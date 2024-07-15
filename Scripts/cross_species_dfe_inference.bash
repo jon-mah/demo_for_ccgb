f32b8f4f5d380b8e05c59efffd7dc9a1d184dfb4
jon-mah <jonmah@uw.edu>
2024-07-15 11:38:10 -0700

    Cross-species DFE, recombination percentile analysis

--- Scripts/cross_species_dfe_inference.bash
+++ Scripts/cross_species_dfe_inference.bash
@@ -5,7 +5,7 @@
 #$ -l h_data=25G
 #$ -l h_rt=72:00:00
 #$ -l highp
-#$ -t 25
+#$ -t 1-40
 #$ -e /u/home/j/jonmah/postproc_error
 #$ -o /u/home/j/jonmah/postproc_output
 #$ -N cross_species_inference
@@ -20,9 +20,10 @@ do
    then
      species=$line
   fi
-done < ../Data/good_species_list.txt
+done < ../SupplementaryAnalysis/supplementary_species_list.txt
 
 # python cross_species_dfe_inference.py ../Analysis/${species}_downsampled_14/core_two_epoch_demography.txt ../Analysis/${species}_downsampled_14/core_empirical_nonsyn_downsampled_sfs.txt ../Analysis/cross_species_dfe/
 # python cross_species_dfe_inference.py ../Analysis/${species}_downsampled_14/core_three_epoch_demography.txt ../Analysis/${species}_downsampled_14/core_empirical_nonsyn_downsampled_sfs.txt ../Analysis/cross_species_dfe/
+# python cross_species_dfe_inference.py ../Analysis/${species}_downsampled_14/accessory_two_epoch_demography.txt ../Analysis/${species}_downsampled_14/accessory_empirical_nonsyn_downsampled_sfs.txt ../Analysis/accessory_cross_species_dfe/
 
-python cross_species_dfe_inference.py ../Analysis/${species}_downsampled_14/accessory_two_epoch_demography.txt ../Analysis/${species}_downsampled_14/accessory_empirical_nonsyn_downsampled_sfs.txt ../Analysis/accessory_cross_species_dfe/
+python cross_species_dfe_inference.py ../SupplementaryAnalysis/${species}/two_epoch_demography.txt ../SupplementaryAnalysis/${species}/core_empirical_nonsyn_downsampled_sfs.txt ../SupplementaryAnalysis/cross_species_dfe/
